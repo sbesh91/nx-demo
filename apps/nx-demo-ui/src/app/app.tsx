0a1b95b225409c8cfc804f50ad55fe233370109c
Steven Beshensky <s.beshensky@gmail.com>
2023-08-03 09:47:28 -0500

    add shared common ui library

--- apps/nx-demo-ui/src/app/app.tsx
+++ apps/nx-demo-ui/src/app/app.tsx
@@ -1,12 +1,37 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-
-import NxWelcome from './nx-welcome';
+import {
+  Banner,
+  Button,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Toggle,
+} from '@nx-demo/common-ui';
 
 export function App() {
   return (
-    <div>
-      <NxWelcome title="nx-demo-ui" />
+    <div className="p-2">
+      <Banner text="Hello World" />
+      <div className="flex gap-2">
+        <Toggle
+          variant="outline"
+          onPressedChange={(pressed) => {
+            if (pressed) {
+              document.documentElement.classList.add('dark');
+            } else {
+              document.documentElement.classList.remove('dark');
+            }
+          }}
+        >
+          B
+        </Toggle>
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button>Open</Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto">Hello World</PopoverContent>
+        </Popover>
+      </div>
     </div>
   );
 }
